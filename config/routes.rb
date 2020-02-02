22b4c32e4ef245a2f95e76afc491300de7d29ffa
wc.kamata <wc_kamata05@yahoo.co.jp>
2020-02-02 10:58:44 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-
   root to: 'toppages#index'
-  resources :microposts
+
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
 end
